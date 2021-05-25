5a3d649043591fa1d0d3aec2278c31a344d2bee4
TrinityInfoFullStackDeveloper <naveen.trinityinfo@gmail.com>
2021-05-25 15:40:55 +0530

    final

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+
+<head>
+
+<meta charset="utf-8" />
+
+<title></title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+<meta name="description" content="" />
+
+<meta name="keywords" content="" />
+
+<!--[if lt IE 9]>
+
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+
+<![endif]-->
+
+<link rel="shortcut icon" href="https://www.smartwebsolutions.org/img/favicon.ico" type="image/x-icon" />
+
+<link rel="icon" href="https://www.smartwebsolutions.org/img/favicon.ico" type="image/x-icon" />
+
+<link type="text/css" rel="stylesheet" href="https://www.smartwebsolutions.org/css/bootstrap.css" />
+
+<link type="text/css" rel="stylesheet" href="https://www.smartwebsolutions.org/font-awesome/css/font-awesome.min.css" />
+
+<link type="text/css" rel="stylesheet" href="https://www.smartwebsolutions.org/css/style.css" />
+
+<link type="text/css" rel="stylesheet" href="https://www.smartwebsolutions.org/css/tools.css" />
+
+<script type="text/javascript" src="https://www.smartwebsolutions.org/js/jquery-1.11.3.min.js" ></script>
+
+<?php //include '../google-analytics-ads.php';?>
+
+</head>
+
+<body>
+
+<?php //include '../header.php';?>
+
+<style>@charset "UTF-8";:root{--bs-blue:#0d6efd;--bs-indigo:#6610f2;--bs-purple:#6f42c1;--bs-pink:#d63384;--bs-red:#dc3545;--bs-orange:#fd7e14;--bs-yellow:#ffc107;--bs-green:#198754;--bs-teal:#20c997;--bs-cyan:#0dcaf0;--bs-white:#fff;--bs-gray:#6c757d;--bs-gray-dark:#343a40;--bs-primary:#0d6efd;--bs-secondary:#6c757d;--bs-success:#198754;--bs-info:#0dcaf0;--bs-warning:#ffc107;--bs-danger:#dc3545;--bs-light:#f8f9fa;--bs-dark:#212529;--bs-font-sans-serif:system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";--bs-font-monospace:SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;--bs-gradient:linear-gradient(180deg,hsla(0,0%,100%,0.15),hsla(0,0%,100%,0));}*,:after,:before{box-sizing:border-box;}@media (prefers-reduced-motion:no-preference){:root{scroll-behavior:smooth;}}body{margin:0;font-family:var(--bs-font-sans-serif);font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#fff;-webkit-text-size-adjust:100%;-webkit-tap-highlight-color:transparent;}</style><link rel="stylesheet" href="styles.9392d677d76aa8585a05.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.9392d677d76aa8585a05.css"></noscript></head>
+
+<section class="team-area-top text-center">
+
+<div class="container">
+
+   <div class="col-md-12 col-sm-12 col-xs-12 toolbox">
+ <h1 style="color: #fff;">Image Data Analyser</h1>
+    
+    <div class="Jumbo">
+    
+    	   /*trinity put your code here*/
+    
+    </div>
+</div>
+
+<div class="col-md-12 col-sm-12 col-xs-12 other-web-tools" style="margin-top: 13px;">
+
+<div class="ofr-div clearfix">
+
+    <h3 class="text-center"> Other Free Web Tools</h3>
+
+   <?php //include '../adsense_code.php';?>
+
+    <?php //include '../other-tools.php';?>
+
+    
+
+ </div>
+
+ <p></p>
+
+</div>
+
+</div>
+
+</section>
+<section class="col-md-12 col-sm-12 col-xs-12 team-area-top text-center company-desc">
+
+  <div class="container">
+
+    <h2>WEB Development COMPANY DUBAI, WEB Development COMPANY KERALA(Kochi)</h2>
+
+    <div class="medium-txt">smartwebsolutions is  website design and Digital marketing company IN DUBAI AND KERALA. We offer complete web-business solutions for all type of businesses of all sizes. 
+
+    We have developed more than 30+ websites in the past Three years and currently provide Search Engine Optimisation (SEO) services to over 10 businesses. 
+
+    We provide excellent online services than any other professional web development and Digital  marketing company in UAE, India.</div>
+
+    <a href="../website-design-company/" class="button-large">Read More</a> 
+
+  </div>
+
+</section>
+
+<app-root></app-root>
+
+<?php 
+
+$title="";
+
+$tags="";
+
+include "../footer.php";
+
+?>
+<script src="runtime.3d091289862144aef75a.js" defer></script><script src="polyfills.62a18a99564201faf539.js" defer></script><script src="scripts.342aae09f187dd3b0381.js" defer></script><script src="main.05f46d2c1db1fca56ce2.js" defer></script>
+</body>
+
+</html>
